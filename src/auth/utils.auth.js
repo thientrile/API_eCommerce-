045ee2e2fba7c38df7f54dbd1c8bbac032a2acbb
LÊ THIÊN TRÍ <thientrile2003@gmail.com>
2024-09-23 12:03:06 +0700

    Update utils.auth.js
    Thay đổi xác thực đang nhập

--- src/auth/utils.auth.js
+++ src/auth/utils.auth.js
@@ -2,17 +2,28 @@
 const { AuthFailureError } = require("../core/error.response");
 const JWT = require("jsonwebtoken");
 const HEADERS = require("../utils/header");
-const { findByClientId } = require("../services/keyToken.services");
+const {
+  findByClientId,
+  deleteByClientId,
+} = require("../services/keyToken.services");
 const createTokenPair = async (payload, publicKey, privateKey) => {
   // accessToken
   const accessToken = await JWT.sign(payload, privateKey, {
     algorithm: "RS256",
     expiresIn: "7 days",
+    header: {
+      typ: "JWT",
+      alg: "RS256",
+    },
   });
 
   const refreshToken = await JWT.sign(payload, privateKey, {
     algorithm: "RS256",
     expiresIn: "14 days",
+    header: {
+      typ: "JWT",
+      alg: "RS256",
+    },
   });
 
   JWT.verify(accessToken, publicKey, (err) => {
@@ -25,29 +36,29 @@ const authertication = async (req, res, next) => {
     //check clientID
     const clientId = req.headers[HEADERS.CLIENT_ID];
     //get accecess token
-    if (!clientId) throw new AuthFailureError(" Invalid client id");
+    if (!clientId) throw new AuthFailureError(" Invalid client");
     const keyStore = await findByClientId(clientId);
 
-    if (!keyStore) throw new AuthFailureError(" Invalid client id");
+    if (!keyStore) throw new AuthFailureError(" Invalid client");
     const userId = keyStore.tk_userId.valueOf();
     // verify refresh token
     const refreshToken = req.headers[HEADERS.REFRESHTOKEN];
     if (refreshToken) {
-      return JWT.verify(
-        refreshToken,
-        keyStore.tk_privateKey,
-        (err, decoded) => {
-          if (err) {
-            throw new AuthFailureError(" Refresh Token has expired");
-          }
-          if (userId !== decoded._id)
-            throw new AuthFailureError(" Invalid User ");
-          req.keyStore = keyStore;
-          req.user = decoded;
-          req.refreshToken = refreshToken;
-          next();
+      if (keyStore.tk_refreshTokensUsed.includes(refreshToken)) {
+        await deleteByClientId(clientId);
+        throw new AuthFailureError("Refresh Token has expired");
+      }
+      return JWT.verify(refreshToken, keyStore.tk_publicKey, (err, decoded) => {
+        if (err) {
+          throw new AuthFailureError("Refresh Token has expired");
         }
-      );
+        if (userId !== decoded._id)
+          throw new AuthFailureError(" Invalid User ");
+        req.keyStore = keyStore;
+        req.user = decoded;
+
+        next();
+      });
     }
     // verify acccesstoken
     const accessToken = req.headers[HEADERS.AUTHORIZATION];

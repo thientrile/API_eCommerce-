adacaad47bc6acb3cd911181a2fe356e56e1adc1
LÊ THIÊN TRÍ <thientrile2003@gmail.com>
2024-09-23 12:13:22 +0700

    update roduct product
    new menthods

--- src/routers/product/index.js
+++ src/routers/product/index.js
@@ -11,9 +11,13 @@ const {
   newProduct,
   updateProduct,
   statusProduct,
+  getlistSpuSeller,
+  getProductDetail,
 } = require("../../controllers/product.controller");
 const { asyncHandler } = require("../../helpers/asyncHandler");
 const { authertication } = require("../../auth/utils.auth");
+const { validateSchema } = require("../../middlewares/joi.middleware");
+const SpuService = require("../../services/spu.service");
 router.use(authertication);
 
 //category
@@ -24,8 +28,9 @@ router.get("/categories", asyncHandler(getListCategory));
 router.post("/brand", asyncHandler(newBrand));
 router.get("/brands", asyncHandler(getListBrand));
 // product
-router.post("", asyncHandler(newProduct));
-router.put("", asyncHandler(updateProduct));
-router.patch("/productStatus",asyncHandler(statusProduct));
-
+router.post("/_new", asyncHandler(newProduct));
+router.put("/_edit/:id", asyncHandler(updateProduct));
+router.patch("/status/:id",asyncHandler(statusProduct));
+router.get("/_seller", asyncHandler(getlistSpuSeller));
+router.get("/_detail/:id", asyncHandler(getProductDetail));
 module.exports = router;

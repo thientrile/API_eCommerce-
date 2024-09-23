7c74e141661e7221f80971e5dbd41ebfed5b1a89
LÊ THIÊN TRÍ <thientrile2003@gmail.com>
2024-09-23 12:11:54 +0700

    Update keyToken.services.js
    remove tk_priviceKey

--- src/services/keyToken.services.js
+++ src/services/keyToken.services.js
@@ -1,8 +1,16 @@
 "use strict";
 
 const keytokenModel = require("../models/keytoken.model");
-const { getErrorMessageMongose, converToUUIDMongoose, convertToObjectIdMongoose } = require("../utils/index");
-const { tk_findOne, tk_deleteOne, tk_updateOne } = require("../repositories/keyToken.repo");
+const {
+  getErrorMessageMongose,
+  converToUUIDMongoose,
+  convertToObjectIdMongoose,
+} = require("../utils/index");
+const {
+  tk_findOne,
+  tk_deleteOne,
+  tk_updateOne,
+} = require("../repositories/keyToken.repo");
 /**
  *
  * @param {String} userId
@@ -13,35 +21,33 @@ const { tk_findOne, tk_deleteOne, tk_updateOne } = require("../repositories/keyT
  * @returns {clientId,token}
  */
 
-const createKeyToken = async ({ userId, publicKey, token, privateKey }) => {
-  const { accessToken } = token;
-
+const createKeyToken = async ({ userId, publicKey }) => {
   const tokens = await keytokenModel.create({
     tk_userId: userId,
     tk_publicKey: publicKey,
-    tk_privateKey: privateKey,
-    tk_accessToken: accessToken,
     tk_refreshTokensUsed: [],
   });
 
-  return tokens
-    ? { clientId: tokens.tk_clientId, publicKey: tokens.publicKey }
-    : null;
+  return tokens ? { clientId: tokens.tk_clientId } : null;
 };
 
 const findByClientId = async (clientId) => {
   return tk_findOne({ tk_clientId: converToUUIDMongoose(clientId) });
 };
 const deleteByClientId = async (clientId) => {
-  return tk_deleteOne({ tk_clientId:converToUUIDMongoose( clientId) });
+  return tk_deleteOne({ tk_clientId: converToUUIDMongoose(clientId) });
 };
 const updateById = async (id, data) => {
-  return tk_updateOne({
-   _id:convertToObjectIdMongoose( id) 
-  },data)}
+  return tk_updateOne(
+    {
+      _id: convertToObjectIdMongoose(id),
+    },
+    data
+  );
+};
 module.exports = {
   createKeyToken,
   findByClientId,
   deleteByClientId,
-  updateById
+  updateById,
 };
